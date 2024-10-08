cd7400fbfe5bdbe02ad117992c792222d797ed0e
HEILLER4 <152053927+HEILLER4@users.noreply.github.com>
2024-10-08 21:37:45 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="test.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./Handle/gone.php" method="post">
+        <input type="text" name="userN" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button type="submit">Sign-up</button>
+    </form>
+</body>
+
+</html>
